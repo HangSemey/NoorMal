4597f98cce1297a3ec1eabf80f21fa533779f77b
HangSemey <huzulkipli@gmail.com>
2025-12-10 12:52:48 +0800

    setup all

---
+++ database/migrations/2025_12_10_042159_create_assets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+    Schema::create('assets', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('user_id');
+        $table->string('type', 50);
+        $table->decimal('value', 12, 2);
+        $table->text('description')->nullable();
+        $table->timestamps(); // created_at & updated_at
+    });
+}
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assets');
+    }
+};
